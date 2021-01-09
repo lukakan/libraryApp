722d3ef183417292a84835a8125f2ab57ac730b5
Lukasz K <kanicki.lukasz@gmail.com>
2021-01-09 15:42:53 +0100

    Class book added, Class library modified.

--- src/Library.java
+++ src/Library.java
@@ -1,20 +1,21 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.1";
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int releaseDate = 2010;
-        int pages = 296;
-        String publisher = "Greg";
-        String isbn = "9788373271890";
+        final String appName = "Biblioteka v0.2";
+        Book book = new Book();
+        book.title = "W pustyni i w puszczy";
+        book.author = "Henryk Sienkiewicz";
+        book.releaseDate = 2010;
+        book.pages = 296;
+        book.publisher = "Greg";
+        book.isbn = "9788373271890";
 
         System.out.println(appName);
         System.out.println("Books avaiable in library:");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book.title);
+        System.out.println(book.author);
+        System.out.println(book.releaseDate);
+        System.out.println(book.pages);
+        System.out.println(book.publisher);
+        System.out.println(book.isbn);
     }
 }
